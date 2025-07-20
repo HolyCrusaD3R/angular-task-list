c7eec55631708725726ec1ce81e4700f15fee134
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2025-07-20 18:53:37 +0400

    add product-item component, conditional rendering

---
+++ src/app/product-item/product-item.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  imports: [],
+  templateUrl: './product-item.html',
+  styleUrl: './product-item.css',
+})
+export class ProductItem {
+  product = input<any>();
+}
--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TaskItem } from '../task-item/task-item';
+import { ProductItem } from '../product-item/product-item';
 
 @Component({
   selector: 'app-task-list',
-  imports: [TaskItem],
+  imports: [TaskItem, ProductItem],
   // templateUrl: './task-list.html',
   // styleUrl: './task-list.css'
   template: `
@@ -14,6 +15,9 @@ import { TaskItem } from '../task-item/task-item';
       </li>
       }
     </ul>
+    @for(product of myProducts; track product.id) {
+    <app-product-item [product]="product" />
+    }
   `,
 })
 export class TaskList {
@@ -21,5 +25,18 @@ export class TaskList {
     { id: 0, description: 'buy groceries', priority: 'high' },
     { id: 1, description: 'go to work', priority: 'high' },
     { id: 2, description: 'walk the dog', priority: 'low' },
+  ];
+
+  myProducts = [
+    {
+      id: 0,
+      src: 'https://m.media-amazon.com/images/I/710o0VupScL._AC_UL320_.jpg',
+      isOutOfStock: false,
+    },
+    {
+      id: 1,
+      src: 'https://m.media-amazon.com/images/I/81D+bCgn8hL._AC_UL320_.jpg',
+      isOutOfStock: true,
+    },
   ];
 }
